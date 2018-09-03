6dca86bd252dec982f4a84ca0707c3741eac61c6
DaanVerschraegen <daan.verschraegen@outlook.com>
2018-09-03 16:17:21 +0200

    server.js aanpassingen

--- server.js
+++ server.js
@@ -13,21 +13,22 @@ app.use(express.static('public'));
 
 //Redirect to eightball
 app.get('/', (req, res) => {
-   res.redirect('/eightball');
+   return res.redirect('/eightball');
 });
 
 //Get answer
 app.post('/eightball', (req, res) => {
 	var question = { question: req.body.question }
-	const possibleAnswers[] = ["it is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no", "Outlook not so good.", "Very doubtful."];
-	string answer;
-	int randomNummer;
+	var possibleAnswers = ["it is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no", "Outlook not so good.", "Very doubtful."];
+	var answer;
+	var randomNummer;
 	
-	getRandomInt(max) {
+	randomNummer = Math.floor(Math.random()*21);
+	/*getRandomInt(max) {
 		return Math.floor(Math.random() * Math.floor(max));
-	}
+	}*/
 	
-	getAnswer(question) {
+	function getAnswer(question) {
 		if (localStorage.getItem(question) === null){
 			randomNummer = getRandomInt(20);
 			answer = possibleAnswers[randomNummer];
