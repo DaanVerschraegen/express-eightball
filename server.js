e8444818bbbf76b38548b30097216cdf79509f02
DaanVerschraegen <daan.verschraegen@outlook.com>
2018-09-03 15:49:48 +0200

    eightball.ejs toegevoegd

--- server.js
+++ server.js
@@ -10,3 +10,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
+
+//Redirect to eightball
+app.get('/', (req, res) => {
+   res.redirect('/eightball');
+});
